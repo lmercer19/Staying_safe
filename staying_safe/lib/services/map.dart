958a9beeaa55a752ae9e281512df57d4897bf978
Lucy Mercer <57599825+lmercer19@users.noreply.github.com>
2021-12-29 22:56:49 +0000

    safe commit, search api working. bug when outputting markers on map

--- staying_safe/lib/services/map.dart
+++ staying_safe/lib/services/map.dart
@@ -6,6 +6,7 @@ import "package:latlong2/latlong.dart";
 import 'package:staying_safe/screens/copyrights_page.dart';
 
 final String apiKey = "RZrPN8h5C4BWs2TaHhBm8akd925h2n0L";
+final List<Marker> markers = List.empty(growable: true);
 
 class MapWidget extends StatelessWidget {
   const MapWidget({Key? key}) : super(key: key);
@@ -14,6 +15,16 @@ class MapWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final canterburyCoords =
         LatLng(51.27597, 1.07561); //update this line to be current location
+
+    final marker = Marker(
+      width: 50.0,
+      height: 55.0,
+      point: canterburyCoords,
+      builder: (BuildContext context) =>
+          const Icon(Icons.location_on, size: 60.0, color: Colors.black),
+    );
+    markers.add(marker);
+
     return MaterialApp(
       title: "TomTom Map",
       home: Scaffold(
@@ -29,25 +40,25 @@ class MapWidget extends StatelessWidget {
                   additionalOptions: {"apiKey": apiKey},
                 ),
                 MarkerLayerOptions(
-                  markers: [
-                    Marker(
-                      width: 80.0,
-                      height: 80.0,
-                      point: LatLng(51.27597, 1.07561),
-                      builder: (BuildContext context) => const Icon(
-                          Icons.location_on,
-                          size: 60.0,
-                          color: Colors.black),
-                    ),
-                  ],
+                  markers: markers,
                 ),
               ],
             ),
             Container(
                 width: 120.0,
                 padding: const EdgeInsets.all(10),
+                alignment: Alignment.bottomLeft,
+                child: Image.asset("images/tt_logo.png")),
+            Container(
+                padding: const EdgeInsets.all(30),
                 alignment: Alignment.topLeft,
-                child: Image.asset("images/tt_logo.png"))
+                child: TextField(
+                  onSubmitted: (value) {
+                    print('$value');
+                    getAddresses(value, canterburyCoords.latitude,
+                        canterburyCoords.longitude);
+                  },
+                ))
           ],
         )),
         floatingActionButton: FloatingActionButton(
@@ -67,9 +78,10 @@ class MapWidget extends StatelessWidget {
 }
 
 /* GET method for copyrights page 
-** @Return JSON response */
+@Return JSON response */
 Future<http.Response> getCopyrightsJSONResponse() async {
-  var url = Uri.parse("https://api.tomtom.com/map/1/copyrights.json?key=$apiKey");
+  var url =
+      Uri.parse("https://api.tomtom.com/map/1/copyrights.json?key=$apiKey");
   var response = await http.get(url);
   return response;
 }
@@ -104,4 +116,31 @@ void parseGeneralCopyrights(jsonResponse, StringBuffer sb) {
     sb.writeln("");
   });
   sb.writeln("");
+}
+
+/* GET method for addresses 
+@param value, the search term
+@param lat, latitude for search bias
+@param lon, longitude for search bias
+ */
+void getAddresses(value, lat, lon) async {
+  final Map<String, String> queryParameters = {'key': '$apiKey'};
+  queryParameters['lat'] = '$lat';
+  queryParameters['lon'] = '$lon';
+
+  var response = await http.get(Uri.https(
+      'api.tomtom.com', '/search/2/search/$value.json', queryParameters));
+  var jsonData = convert.jsonDecode(response.body);
+  print('$jsonData');
+  var results = jsonData['results'];
+  for (var element in results) {
+    var position = element['position'];
+    var marker = Marker(
+        point: LatLng(position['lat'], position['lon']),
+        width: 50.0,
+        height: 50.0,
+        builder: (BuildContext context) =>
+            const Icon(Icons.location_on, size: 40.0, color: Colors.blue));
+    markers.add(marker);
+  }
 }
